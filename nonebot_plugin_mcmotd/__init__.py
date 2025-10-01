6bf79ec0b4098d33c79fe96b917da24f2ec598b0
AquaOH <2863896505@qq.com>
2025-10-01 18:25:34 +0800

    fix: 更新版本号至1.0.1

--- nonebot_plugin_mcmotd/__init__.py
+++ nonebot_plugin_mcmotd/__init__.py
@@ -2,7 +2,7 @@ from nonebot.plugin import PluginMetadata
 
 from .config import Config
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __plugin_meta__ = PluginMetadata(
     name="Minecraft MOTD 查询",
